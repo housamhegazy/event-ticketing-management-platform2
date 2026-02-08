60b4bc6cc03171ce8c51911a84487ddaac3d542a
housamhegazy <geohousam@gmail.com>
2026-02-08 23:25:48 +0300

    update

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import { Provider } from 'react-redux';
 import { store } from './Redux/store.js';
 
